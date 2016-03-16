b30347220ae93cecf0903156251cf3b9dc5971b9
Tim Ulman <timulman@gmail.com>
2016-03-15 20:42:13 -0400

    added oages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def info
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+end
